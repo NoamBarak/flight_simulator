394da3f3980186728e271daf920d840d8cbacc6c
KarinShimel <karin.shimel@gmail.com>
2019-12-21 15:36:01 +0200

    Server Working

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ void runServer(unsigned short portShort) {
         std::cerr << "Error in accepting client" << endl;
         exit(-4);
     }*/
-    //create socket
+    /*//create socket
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketfd == -1) {
         //error
@@ -120,12 +120,63 @@ void runServer(unsigned short portShort) {
     if (client_socket == -1) {
         std::cerr << "Error accepting client" << std::endl;
         exit(-4);
-    }
+    }else
+        cout<<"Accepted client"<<endl;
 
     close(socketfd); //closing the listening socket
 
     //reading from client
     while (true) {
+        char buffer[1024] = {0};
+        int valread = read(client_socket, buffer, 1024);
+        std::cout << buffer << std::endl;
+    }*/
+    //create socket
+    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1) {
+        //error
+        std::cerr << "Could not create a socket"<<std::endl;
+        exit (-1);
+    }
+
+    //bind socket to IP address
+    // we first need to create the sockaddr obj.
+    sockaddr_in address; //in means IP4
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; //give me any IP allocated for my machine
+    address.sin_port = htons(portShort);
+    //we need to convert our number
+    // to a number that the network understands.
+
+    //the actual bind command
+    if (bind(socketfd, (struct sockaddr *) &address, sizeof(address)) == -1) {
+        std::cerr<<"Could not bind the socket to an IP"<<std::endl;
+        exit (-2);
+    }
+
+    //making socket listen to the port
+
+    if (listen(socketfd, 5) == -1) { //can also set to SOMAXCON (max connections)
+        std::cerr<<"Error during listening command"<<std::endl;
+        exit (-3);
+    } else{
+        std::cout<<"Server is now listening ..."<<std::endl;
+    }
+
+    // accepting a client
+    sockaddr_in Caddress;
+    int client_socket = accept(socketfd, (struct sockaddr *)&Caddress,
+                               (socklen_t*)&Caddress);
+
+    if (client_socket == -1) {
+        std::cerr<<"Error accepting client"<<std::endl;
+        exit (-4);
+    }
+
+    close(socketfd); //closing the listening socket
+
+    //reading from client
+    while(true) {
         char buffer[1024] = {0};
         int valread = read(client_socket, buffer, 1024);
         std::cout << buffer << std::endl;
@@ -274,8 +325,8 @@ int main() {
     //std::cout << typeid(c1).name() << '\n';
 
 */
-    if (c != NULL)
-        i += c->execute(fileVector, i);
+    //if (c != NULL)
+       // i += c->execute(fileVector, i);
 
 
     OpenServerCommand *openServerCommand = new OpenServerCommand;
@@ -322,8 +373,8 @@ int main() {
 
     // Karin Testing line 323
 
-
-
+    OpenServerCommand *openServerCommand1 = new OpenServerCommand;
+    i += openServerCommand1->execute(fileVector, i);
 
 
 
